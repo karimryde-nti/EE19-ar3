bad98d79c17ce8885c7b8f0ca514aaafcf9ddeb2
karye <karye.webb@gmail.com>
2021-09-06 14:05:00 +0200

    Börjat med lite PHP

---
+++ grunder/test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Testa lite PHP</h1>
+    <?php
+    echo "<p>Mitt namn är Karim.<br>";
+    echo "Jag bor i Bromma.</p>";
+
+    echo "<p>";
+    echo date("l d F Y");
+    echo "</p>";
+
+    echo "<p>";
+    setlocale(LC_ALL, "sv_SE.utf8");
+    echo strftime("%A %e %B %Y");
+    echo "</p>";
+    ?>
+</body>
+</html>
---
+++ grunder/uppgift1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="../bootstrap.min.css">
+</head>
+<body>
+    <h1>Uppgift 1</h1>
+    <?php
+    // Två variabler för namn och adress
+    $namn = "Karim";
+    $adress = "Follingbogatan 26";
+
+    // Skriv ut "Mitt namn är ..., jag bor på ...
+    echo "Mitt namn är $namn, jag bor på $adress.<br>";
+
+    // Variabler för kön och ålder
+    $kön = "man";
+    $ålder = 18;
+
+    // Skriv ut "Jag är .. och jag är .. år."
+    echo "Jag är en $kön och jag är $ålder år.<br>";
+    ?>
+</body>
+</html>
