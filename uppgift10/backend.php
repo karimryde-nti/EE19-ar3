ab88053329493e80b85beb1caccd037b24ee97e6
Karim Ryde <karye.webb@gmail.com>
2021-10-05 13:34:37 +0200

    Tränat form, bootstrap, name, PHP

---
+++ uppgift10/backend.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Temperaturomvandlare</title>
+    <link rel="stylesheet" href="../bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <h3>Hur varmt är det?</h3>
+        <?php
+        $temp = filter_input(INPUT_POST, 'temp', FILTER_SANITIZE_STRING);
+        $riktning = filter_input(INPUT_POST, 'riktning', FILTER_SANITIZE_STRING);
+
+        echo "<p>Temperaturen blir $temp&deg;</p>";
+
+        // Om riktning är c->f 
+        if ($riktning == "cf") {
+            $farenheit = $temp * 1.8 + 32;
+            echo "<p>$temp&deg; Celsius är $farenheit&deg; Farenheit</p>";
+        } else {
+            $celsius = ($temp - 32) / 1.8;
+            echo "<p>$temp&deg; Farenheit är $celsius&deg; Celsius</p>";
+        }
+
+        ?>
+    </div>
+</body>
+</html>
---
+++ uppgift10/frontend.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Temperaturomvandlare</title>
+    <link rel="stylesheet" href="../bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <h3>Hur varmt är det?</h3>
+        <form action="./backend.php" method="post">
+            <div class="mb-3">
+                <label for="temp" class="form-label">Ange temperatur</label>
+                <input type="text" class="form-control" id="temp" name="temp">
+            </div>
+            <div class="kol2">
+                <p>Ange riktning</p>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="cf" checked name="riktning" value="cf">
+                    <label class="form-check-label" for="cf">C&deg; till F&deg;</label>
+                </div>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="fc" name="riktning" value="fc">
+                    <label class="form-check-label" for="fc">F&deg; till C&deg;</label>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
