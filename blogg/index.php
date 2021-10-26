6846d8db9ba351fac844f29d378b1a78f35c8d8c
Karim Ryde <karye.webb@gmail.com>
2021-10-26 12:37:01 +0200

    Börjat bloggen med inloggning

---
+++ blogg/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Min enkla blogg</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/minty/bootstrap.min.css">
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <header>
+            <h1>Karims blogg</h1>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a class="active nav-link" href="#">Mina inlägg</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./admin/admin.php">Admin</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            ...
+        </main>
+        <footer>
+            Hösten 2021
+        </footer>
+    </div>
+</body>
+</html>
