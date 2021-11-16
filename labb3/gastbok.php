c3524e9a9a13339524114c1d8af081921eceaf35
Karim Ryde <karye.webb@gmail.com>
2021-11-16 13:59:10 +0100

    Testar deploy på Azure

--- labb3/gastbok.php
+++ labb3/gastbok.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Gästboken</title>
-    <link rel="stylesheet" href="../bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="kontainer">
